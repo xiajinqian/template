cf686d421ab6d7d6cedc9cf0686e022de12cfd35
xiajinqian <xia.jinqian@163.com>
2018-02-28 17:53:14 +0800

    jdk版本设置

---
+++ admin/src/main/webapp/static/js/controller/index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Administrator on 2018/2/28.
+ */
+var app = angular.module('myApp', []);
+
+app.controller('myCtrl', function($scope) {
+    $scope.firstName = "John";
+    $scope.lastName = "Doe";
+    $scope.indexVlue="java test";
+
+});
